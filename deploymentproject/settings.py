404c3866acd9de00b24bc06000d9465d9c1a5793
pooja8857 <reddypooja308@gmail.com>
2020-09-25 09:33:54 +0530

    first commit

--- deploymentproject/settings.py
+++ deploymentproject/settings.py
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'deploymentproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'herokudb',
+        'NAME': 'deployedb',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': 3306,
+    },
+    'sqlt': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
